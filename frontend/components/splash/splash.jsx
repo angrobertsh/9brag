ff8b7adb800a74d61dda07e6f64b36ce83df6ba6
Robert Ang <angrobertsh@gmail.com>
2016-09-15 19:27:24 -0700

    cleanup

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -8,7 +8,7 @@ class Splash extends React.Component{
   }
 
   render() {
-    return (<div className="splashcontainercontainer"><Link to="/memes"><div className="splashcontainer"><p><img src="http://res.cloudinary.com/dujcpxlhk/image/upload/v1473100469/f9gvavsmmgeemiyun6jp.png" />9Brag</p><p className="mini">The Large Feet of The Internet</p></div></Link></div>);
+    return (<div className="splashcontainercontainer"><Link to="/memes"><div className="splashcontainer"><p><img src="http://res.cloudinary.com/dujcpxlhk/image/upload/v1473100469/f9gvavsmmgeemiyun6jp.png" />9Brag</p><p className="mini">The Feet of The Internet</p></div></Link></div>);
   }
 
 }
