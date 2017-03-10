9230b1a40597cb5f56269938504873b4b3c5d1da
Robert Ang <angrobertsh@gmail.com>
2017-03-09 18:55:48 -0800

    trying to minify react still

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/entry.jsx",
+  entry: "./frontend/9brag.jsx",
   output: {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js",
